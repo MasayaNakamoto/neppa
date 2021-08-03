4ebaaa7e888b1a75e21403bcdd24402106986e0e
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-08-03 05:49:32 +0000

    ユーザー側のコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'shops/index'
+  get 'shops/show'
+  get 'orders/new'
+  get 'orders/confirm'
+  get 'orders/complete'
+  get 'orders/index'
+  get 'orders/show'
+  get 'customers/show'
+  get 'addresses/index'
+  get 'cart_items/index'
+  get 'items/index'
+  get 'items/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'homes/about'  => 'homes#about'
