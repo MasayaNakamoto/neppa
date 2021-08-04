533ea92ea33fadceaf7703e68ec30d188296149e
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-08-04 07:27:17 +0000

    コントローラ前準備

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,10 +1,39 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
 
-  def new
-  end
+ def new
+    @customer = Customer
+    @item = Item.new
+ end
 
-  def show
-  end
+ def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+      flash[:success] = "商品登録完了"
+    else
+      render :new
+    end
+ end
+
+ def index
+    @customer = Customer
+    @items = Item.order(id: :desc).page(params[:page]).reverse_order
+ end
+
+ def show
+    @customer = Customer
+    @item = Item.find(params[:id])
+ end
+
+ def edit
+    @customer = Customer
+    @item = Item.find(params[:id])
+ end
+
+ def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item)
+ end
+
 end
