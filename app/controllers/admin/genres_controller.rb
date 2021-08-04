3a3fc8d9c0ec2bcd28288f01bb5d138e2b3eeb47
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-08-04 07:34:58 +0000

    商品登録と店舗登録に必要な下準備

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,4 +1,28 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
+    @customer = Customer
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+    @customer = Customer
+  end
+
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 end
--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -1,10 +1,38 @@
 class Admin::ShopsController < ApplicationController
-  def index
-  end
-
   def new
+    @customer = Customer
+    @shop = Shop.new
   end
 
-  def show
-  end
+ def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to admin_shops_path
+      flash[:success] = "店舗登録完了"
+    else
+      render :new
+    end
+ end
+
+ def index
+    @customer = Customer
+    @shops = Shop.order(id: :desc).page(params[:page]).reverse_order
+ end
+
+ def show
+    @customer = Customer
+    @shop = Shop.find(params[:id])
+ end
+
+ def edit
+    @customer = Customer
+    @shop = Shop.find(params[:id])
+ end
+
+ def update
+    shop = Shop.find(params[:id])
+    shop.update(shop_params)
+    redirect_to admin_shop_path(item)
+ end
+
 end
