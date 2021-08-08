8ff27fa0fce4b58220796b0679ee6e599b1e6b54
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-08-08 03:47:30 +0000

    注文情報入力画面の作成中。次回配送先登録機能の実装を行う

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
   def new
+    @customer = Customer.find(current_customer.id)
+    @order = Order.new
   end
 
   def confirm
+  end
+
+  def create
   end
 
   def complete
@@ -12,5 +17,21 @@ class OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :post_code, :address, :name, :shipping_cost, :total_price, :payment_method, :status)
+
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+  end
+
+  def address_params
+    params.require(:order).permit(:post_code, :address, :name)
+
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '19eaa69724d571b513c3ab7a2cccd24590a1da2542204069b53694fd171ee2a146428ab749cf22bc73a902edc2de3f18cacb5449603284aaffb4a24abdf828e8'
