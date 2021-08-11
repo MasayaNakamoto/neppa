0fc943357fa56278bf015285064b7c8234a31872
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-08-11 08:03:05 +0000

    いいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ get 'homes/about'  => 'homes#about'
   resources :items, only: [:index, :show]
   resources :shops, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
   resources :cart_items, only: [:index, :create, :destroy, :update] do
