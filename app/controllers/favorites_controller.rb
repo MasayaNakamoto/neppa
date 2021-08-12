47fed1202db5b80588b3a69d5f49efbceabdc340
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-08-12 04:19:08 +0000

    非同期通信の実装中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     shop = Shop.find(params[:shop_id])
     favorite = current_customer.favorites.new(shop_id: shop.id)
     favorite.save
-    redirect_to shop_path(shop)
+    # redirect_to shop_path(shop)
   end
 
   def destroy
     shop = Shop.find(params[:shop_id])
     favorite = current_customer.favorites.find_by(shop_id: shop.id)
     favorite.destroy
-    redirect_to shop_path(shop)
+    # redirect_to shop_path(shop)
   end
 end
