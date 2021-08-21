1552c1cd9ff008564b28f64a83fafdc085ad0480
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-08-21 08:02:15 +0000

    genre.nameの表示完了とsplashlogoの表示完了

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,6 +4,7 @@ class ShopsController < ApplicationController
   def index
     @shops = Shop.order(id: :desc).page(params[:page]).reverse_order
     @genres = Genre.all
+
     # 上のやつはジャンル名表示に困ってて残してる現状
   end
 
