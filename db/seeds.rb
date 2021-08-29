786936eba57bf5182734688665d7ae46b73f2f21
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-08-29 06:27:32 +0000

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ Customer.create!(
   first_name:"太郎",
   last_name_kana:"ヤマダ",
   first_name_kana:"タロウ",
-  phone_number:"00000000")
+  phone_number:"0000000")
 Customer.create!(
   email:"test2@mail.com",
   password:"password",
@@ -27,7 +27,7 @@ Customer.create!(
   first_name:"花子",
   last_name_kana:"サトウ",
   first_name_kana:"ハナコ",
-  phone_number:"00000000")
+  phone_number:"0000000")
 Customer.create!(
   email:"test3@mail.com",
   password:"password",
@@ -35,7 +35,7 @@ Customer.create!(
   first_name:"いいい",
   last_name_kana:"アアア",
   first_name_kana:"イイイ",
-  phone_number:"00000000")
+  phone_number:"0000000")
 
 
   [
@@ -58,14 +58,14 @@ Genre.create!(name: "兵庫")
 Genre.create!(name: "奈良")
 Genre.create!(name: "和歌山")
 
-Shop.create!(genre_id: 1, name: "みやこ温泉", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "京都府京都市山科区東野中井ノ上町", phone_number: 11112345678, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
-Shop.create!(genre_id: 2, name: "まいどの湯", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "大阪府大阪市北区豊崎", phone_number: 11112345678, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
-Shop.create!(genre_id: 3, name: "みずうみ湯", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "滋賀県近江八幡市長命寺町", phone_number: 11112345678, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
-Shop.create!(genre_id: 4, name: "サウナ&スパこーべ", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "兵庫県尼崎市", phone_number: 11112345678, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
-Shop.create!(genre_id: 5, name: "しか温泉", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "奈良県大和郡山市", phone_number: 11112345678, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
-Shop.create!(genre_id: 6, name: "湯処みかん", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "和歌山県和歌山市", phone_number: 11112345678, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
-Shop.create!(genre_id: 1, name: "まいこの湯", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "京都府京都市北区", phone_number: 11112345678, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
-Shop.create!(genre_id: 3, name: "SPA レイク", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "滋賀県草津市", phone_number: 11112345678, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
+Shop.create!(genre_id: 1, name: "みやこ温泉", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "京都府京都市山科区東野中井ノ上町", phone_number: 1111234567, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
+Shop.create!(genre_id: 2, name: "まいどの湯", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "大阪府大阪市北区豊崎", phone_number: 1111234567, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
+Shop.create!(genre_id: 3, name: "みずうみ湯", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "滋賀県近江八幡市長命寺町", phone_number: 1111234567, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
+Shop.create!(genre_id: 4, name: "サウナ&スパこーべ", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "兵庫県尼崎市", phone_number: 1111234567, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
+Shop.create!(genre_id: 5, name: "しか温泉", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "奈良県大和郡山市", phone_number: 1111234567, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
+Shop.create!(genre_id: 6, name: "湯処みかん", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "和歌山県和歌山市", phone_number: 1111234567, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
+Shop.create!(genre_id: 1, name: "まいこの湯", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "京都府京都市北区", phone_number: 1111234567, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
+Shop.create!(genre_id: 3, name: "SPA レイク", introduction: "※こちらはポートフォリオサイトのため架空のお店です", price: 1000, postal_code: 1234567, address: "滋賀県草津市", phone_number: 1111234567, homepage: "https://twitter.com/04_nakamoto04/", sauna: "サウナの温度は95度で屋外に設置。水風呂も屋外に設置されており、温度は12度。外気浴が出来るよう畳のコーナーあり。", image: File.open("app/assets/images/sauna.jpg"))
 
 
 
